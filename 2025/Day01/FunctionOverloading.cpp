aa7501c882182240b21073b7909197c18302ad03
ParkSeongHyeon <lovehyun95@naver.com>
2025-08-02 23:41:15 +0900

    C++ Day1 예제 : NameSpace3까지

---
+++ 2025/Day01/FunctionOverloading.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void MyFunc(void) {
+	std::cout<<"MyFunc(void) called"<<std::endl;
+}
+
+void MyFunc(char c) {
+	std::cout << "MyFunc(char c) called" << std::endl;
+}
+
+void MyFunc(int a, int b) {
+	std::cout << "MyFunc(int a, int b) called" << std::endl;
+}
+
+int main(void)
+{
+	MyFunc();
+	MyFunc('a');
+	MyFunc(1, 2);
+	return 0;
+}
